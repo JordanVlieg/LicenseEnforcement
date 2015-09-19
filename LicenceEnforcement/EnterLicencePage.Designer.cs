6e2bcc6d7ab7e62014269ca9e672f3d7f83c7b1b
jordanvlieg <jordanvlieg@gmail.com>
2015-09-19 15:09:54 -0700

    Initial Commit. -Creates license keys and signs them -Verifies signed license keys from a file -Rough UI completed.

---
+++ LicenceEnforcement/EnterLicencePage.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace LicenceEnforcement
+{
+    partial class EnterLicencePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.lblInvalid = new System.Windows.Forms.Label();
+            this.lblValid = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuy.Location = new System.Drawing.Point(346, 193);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(126, 37);
+            this.btnBuy.TabIndex = 5;
+            this.btnBuy.Text = "Buy a License";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(181, 157);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(138, 37);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start Program";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(48, 106);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(35, 20);
+            this.txtMonth.TabIndex = 0;
+            this.txtMonth.TextChanged += new System.EventHandler(this.txtMonth_TextChanged);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(263, 105);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(175, 20);
+            this.txtKey.TabIndex = 1;
+            this.txtKey.TextChanged += new System.EventHandler(this.txtKey_TextChanged);
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(168, 105);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(70, 20);
+            this.txtYear.TabIndex = 2;
+            this.txtYear.TextChanged += new System.EventHandler(this.txtYear_TextChanged);
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(108, 106);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(35, 20);
+            this.txtDay.TabIndex = 3;
+            this.txtDay.TextChanged += new System.EventHandler(this.txtDay_TextChanged);
+            // 
+            // lblInvalid
+            // 
+            this.lblInvalid.AutoSize = true;
+            this.lblInvalid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvalid.Location = new System.Drawing.Point(197, 69);
+            this.lblInvalid.Name = "lblInvalid";
+            this.lblInvalid.Size = new System.Drawing.Size(115, 17);
+            this.lblInvalid.TabIndex = 13;
+            this.lblInvalid.Text = "License is Invalid";
+            this.lblInvalid.Visible = false;
+            // 
+            // lblValid
+            // 
+            this.lblValid.AutoSize = true;
+            this.lblValid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValid.Location = new System.Drawing.Point(197, 69);
+            this.lblValid.Name = "lblValid";
+            this.lblValid.Size = new System.Drawing.Size(106, 17);
+            this.lblValid.TabIndex = 7;
+            this.lblValid.Text = "License is Valid";
+            this.lblValid.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(89, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(149, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(244, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "-";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(146, 32);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(208, 24);
+            this.lblInstructions.TabIndex = 4;
+            this.lblInstructions.Text = "Enter Your License Key";
+            // 
+            // EnterLicencePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 242);
+            this.Controls.Add(this.lblInvalid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblValid);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtMonth);
+            this.Name = "EnterLicencePage";
+            this.Text = "Licence Confirmation";
+            this.Load += new System.EventHandler(this.EnterLicencePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.Label lblInvalid;
+        private System.Windows.Forms.Label lblValid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ LicenceEnforcement/LicenseConfirm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LicenseEnforcement
+{
+    internal class LicenseConfirm
+    {
+        //private RSACryptoServiceProvider GLOBAL_RSA;
+
+        public static void GenerateLicense(int months)
+        {
+            //must be 20 bytes longs
+            string licenseKey = DateTime.Now.Date.AddMonths(months).ToShortDateString();
+            //Debug.WriteLine(testing);
+            string machineName = System.Environment.MachineName;
+            licenseKey += machineName;
+            while (licenseKey.Length < 20)
+            {
+                licenseKey += "0";
+            }
+            licenseKey = licenseKey.Substring(0, 20);
+
+            byte[] byteLicenseKey = Encoding.ASCII.GetBytes(licenseKey);
+            byte[] signedKey;
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+
+            string publicKey = RSA.ToXmlString(false);
+
+            XmlDocument licenseDocument = new XmlDocument();
+
+            XmlElement parentNode = licenseDocument.CreateElement("LicenseInfo");
+
+            licenseDocument.AppendChild(parentNode);
+
+
+
+            XmlElement xmlDocLicenseKey = licenseDocument.CreateElement("KEY");
+
+            xmlDocLicenseKey.InnerText = Encoding.Default.GetString(byteLicenseKey);
+
+            parentNode.AppendChild(xmlDocLicenseKey);
+
+            RSAPKCS1SignatureFormatter rsaFormatter = new RSAPKCS1SignatureFormatter(RSA);
+
+            rsaFormatter.SetHashAlgorithm("SHA1");
+
+            signedKey = rsaFormatter.CreateSignature(byteLicenseKey);
+
+            XmlElement xmlDocLicenseSignature = licenseDocument.CreateElement("SignedKey");
+
+            xmlDocLicenseSignature.InnerText = Convert.ToBase64String(signedKey);
+
+            parentNode.AppendChild(xmlDocLicenseSignature);
+
+            XmlDocumentFragment publicKeyNode = licenseDocument.CreateDocumentFragment();
+            publicKeyNode.InnerXml = publicKey;
+
+            parentNode.AppendChild(publicKeyNode);
+
+            licenseDocument.Save(Environment.CurrentDirectory + @"\licenseinfo.xml");
+            
+
+        }
+
+        
+        public static bool VerifyLicense()
+        {
+            
+
+
+            XmlDocument licenseFile = new XmlDocument();
+            licenseFile.Load(Environment.CurrentDirectory + @"\licenseinfo.xml");
+
+            //XmlNode RSAInfo = licenseFile.DocumentElement.SelectSingleNode(@"/LicenseInfo/RSAKeyValue/Modulus");
+
+
+
+            RSAParameters RSAKeyInfo = new RSAParameters();
+            RSAKeyInfo.Modulus = Convert.FromBase64String(licenseFile.DocumentElement.SelectSingleNode(@"/LicenseInfo/RSAKeyValue/Modulus").InnerText);
+            RSAKeyInfo.Exponent = Convert.FromBase64String(licenseFile.DocumentElement.SelectSingleNode(@"/LicenseInfo/RSAKeyValue/Exponent").InnerText);
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+
+            //RSA = this.GLOBAL_RSA;
+
+            RSA.ImportParameters(RSAKeyInfo);
+
+            //Debug.WriteLine(RSAInfo.SelectSingleNode("/Modulus").InnerText);
+            RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(RSA);
+
+            rsaDeformatter.SetHashAlgorithm("SHA1");
+            byte[] hash =
+                Encoding.ASCII.GetBytes(licenseFile.DocumentElement.SelectSingleNode(@"/LicenseInfo/KEY").InnerText);
+            byte[] sig =
+                Convert.FromBase64String(
+                    licenseFile.DocumentElement.SelectSingleNode(@"/LicenseInfo/SignedKey").InnerText);
+            if (rsaDeformatter.VerifySignature(hash, sig))
+            {
+                return true;
+                Debug.WriteLine("SUCCESS");
+            }
+            return false;
+
+        }
+
+    }
+}
